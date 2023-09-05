c4109b5dc54c437c4494ab20d44b54e44b3f9a2e
Francesco <francescomarchese91@gmail.com>
2023-09-05 21:53:59 +0200

    Python giorno 1

---
+++ Modulo 4/D1 – Introduzione alla programmazione e a Python.py
@@ -0,0 +1,31 @@
+## 1.
+
+Studenti = 25
+
+## 2.
+
+print ("La prima variabile: " + str(Studenti))
+
+## 3.
+
+nuovi_studenti = 3
+
+## 4.
+
+somma_studenti = Studenti + nuovi_studenti
+
+print("La somma delle variabili è: " + str(somma_studenti))
+
+## 5.
+
+lista_numeri = [1,2,3,4,5]
+
+print("La lista de numeri è: " + str(lista_numeri))
+
+## 6.
+
+lista_film = ["La leggenda del pianista sull'oceano",
+              "Non è un pese per vecchi",
+              "Eternal sunshine of the spotless mind"]
+
+print("La lista dei film è: " + str(lista_film))
